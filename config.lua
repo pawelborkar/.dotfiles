cca420984775e02daee3888a454ea9df45b4f2f5
Pawel Borkar <36134699+pawelborkar@users.noreply.github.com>
2023-11-25 00:42:59 +0530

    add: floating terminal options

--- config.lua
+++ config.lua
@@ -5,6 +5,14 @@ lvim.plugins = {
   { "pocco81/auto-save.nvim" }
 }
 
+-- Floating Terminal Commands
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Terminal",
+  f = { "<cmd>ToggleTerm<cr>", "Floating terminal" },
+  v = { "<cmd>2ToggleTerm size=30 direction=vertical<cr>", "Split vertical" },
+  h = { "<cmd>2ToggleTerm size=30 direction=horizontal<cr>", "Split horizontal" },
+}
+
 -- formatters
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
